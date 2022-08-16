b15deba2a878893b2fc789034c6996464537342c
Hamid <www.abdulhamid3311@gmail.com>
2022-08-16 21:47:24 +0600

    push for heroku

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors());
 app.use(express.json());
-
 
 
 
